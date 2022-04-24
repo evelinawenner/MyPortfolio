ff87cf36a966155645553d26f92dd51ac35f6ada
Evelina Wenner <evelina.nilssonvenner@medieinstitutet.se>
2022-04-24 16:14:35 +0200

    languageContextProvider in App

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,19 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { LandingPage } from "./components/pages/landingPage/LandingPage";
 import ScrollToTop from "./components/globals/ScrollToTop";
+import { LanguageContextProvider } from "./components/globals/context/languageContext/LanguageContext";
 
 function App() {
   return (
-    //Page components here
-    <Router>
-      <ScrollToTop />
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-      </Routes>
-    </Router>
+    <LanguageContextProvider>
+      {/* Page components here */}
+      <Router>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+        </Routes>
+      </Router>
+    </LanguageContextProvider>
   );
 }
 
--- src/components/pages/landingPage/LandingPage.tsx
+++ src/components/pages/landingPage/LandingPage.tsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
+import { LanguageContext } from "../../globals/context/languageContext/LanguageContext";
 import { MainHeading, P } from "../../globals/styledElements/Text";
 import { Container } from "./LandingPageStyle";
 
 export const LandingPage = () => {
+  const lang = useContext(LanguageContext);
   return (
     <Container>
       <div className="hero">
-        <MainHeading>Evelina Wenner</MainHeading>
-        <P>Portfolio</P>
+        <MainHeading>{lang.language.language.my_name}</MainHeading>
+        <P>{lang.language.language.portfolio}</P>
       </div>
     </Container>
   );
